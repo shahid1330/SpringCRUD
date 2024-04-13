16d40cf77fedba02cb88fbe10797887ff6dadd6b
mdshahid <87535723+shahid1330@users.noreply.github.com>
2024-04-13 10:43:03 +0530

    Added SpringCRUD code

---
+++ student/src/main/java/bca_bscit6/student/App.java
@@ -0,0 +1,13 @@
+package bca_bscit6.student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
